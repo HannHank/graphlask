f91ff15eadb2e4df8b6a0a9dc85fce12bc8ab1d8
HannHank <hanke.johann@gmail.com>
2021-05-21 00:03:50 +0200

    initial commit

---
+++ books/types/characters.py
@@ -0,0 +1,11 @@
+
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyObjectType
+
+from ..models.characters import Characters as CharactersModel
+
+
+class Characters(SQLAlchemyObjectType):
+    class Meta:
+        model = CharactersModel
+        interfaces = (relay.Node,)
